3f63af7cb66df3b554c953f24ff8994ce6001ca4
cjd6568358 <cjd6568358@163.com>
2018-11-20 11:34:35 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,7 @@ module.exports = {
             runtimeCaching: [
                 // 配置路由请求缓存
                 {
-                    urlPattern: /\/xgj\/.*.+?\.?[^.]{4,}$/, // 匹配文件
+                    urlPattern: /\/xgj\/my$/, // 匹配文件
                     handler: 'networkFirst' // 网络优先
                 }
             ]
