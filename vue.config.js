55edfa73bf2276c1949d12d818ca1195fa1cf00d
cjd6568358 <cjd6568358@163.com>
2018-11-20 14:56:20 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
                     handler: 'networkFirst', // 网络优先
                     options: {
                         cacheableResponse: {
-                            statuses: [0, 503]
+                            statuses: [200, 404]
                         }
                     }
                 }
